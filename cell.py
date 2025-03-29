dac8081a072a485881fd65d93f10944141efdc22
Tishues <BradleyBounds@outlook.com>
2025-03-28 23:25:03 -0500

    D: Cell draw_move() method & adjustments to main() to test

--- cell.py
+++ cell.py
@@ -12,20 +12,36 @@ class Cell(Window):
         self._y2 = None
         self._win = _win
 
-    def draw(self, _x1, _y1, _x2, _y2):
-        self._x1 = _x1
-        self._x2 = _x2
-        self._y1 = _y1
-        self._y2 = _y2
+    def draw(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
         if self.has_left_wall:
-            line = Line(Point(_x1, _y1), Point(_x1, _y2))
+            line = Line(Point(x1, y1), Point(x1, y2))
             self._win.draw_line(line)
         if self.has_top_wall:
-            line = Line(Point(_x1, _y1), Point(_x2, _y1))
+            line = Line(Point(x1, y1), Point(x2, y1))
             self._win.draw_line(line)
         if self.has_right_wall:
-            line = Line(Point(_x2, _y1), Point(_x2, _y2))
+            line = Line(Point(x2, y1), Point(x2, y2))
             self._win.draw_line(line)
         if self.has_bottom_wall:
-            line = Line(Point(_x1, _y2), Point(_x2, _y2))
+            line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        half_cell = abs(self._x1 - self._x2) // 2
+        x_mid = half_cell + self._x1
+        y_mid = half_cell + self._y1
+
+        half_cell2 = abs(to_cell._x1 - to_cell._x2) // 2
+        x_mid2 = half_cell2 + to_cell._x1
+        y_mid2 = half_cell2 + to_cell._y1
+
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+
+        line = Line(Point(x_mid, y_mid), Point(x_mid2, y_mid2))
+        self._win.draw_line(line, fill_color)
--- main.py
+++ main.py
@@ -3,23 +3,45 @@ from cell import Cell
 
 def main():
     win = Window(800, 600)
-    cell = Cell(win)
-    cell.draw(150,200, 200,250)
-    cell = Cell(win)
-    cell.draw(200,200, 250,250)
-    cell = Cell(win)
-    cell.draw(250,200, 300,250)
-    cell = Cell(win)
-    cell.draw(200,150, 250,200)
-    cell = Cell(win)
-    cell.draw(200,250, 250,300)
-    cell = Cell(win)
-    cell.draw(200,300, 250,350)
-    cell = Cell(win)
-    cell.draw(100,100, 350,400)
-    cell = Cell(win)
-    #l = Line(Point(50, 50), Point(400,400))
-    #win.draw_line(l, "black")
+
+    cell1 = Cell(win)
+    cell1.has_right_wall = False
+    cell1.draw(50,50, 100,100)
+
+    cell2 = Cell(win)
+    cell2.has_left_wall = False
+    cell2.has_right_wall = False
+    cell2.draw(100,50, 150,100)
+
+    cell1.draw_move(cell2)
+
+    cell3 = Cell(win)
+    cell3.has_left_wall = False
+    cell3.has_bottom_wall = False
+    cell3.draw(150,50, 200,100)
+
+    cell2.draw_move(cell3)
+
+    cell4 = Cell(win)
+    cell4.has_top_wall = False
+    cell4.has_left_wall = False
+    cell4.draw(150,100, 200,150)
+
+    cell3.draw_move(cell4)
+
+    cell5 = Cell(win)
+    cell5.has_right_wall = False
+    cell5.has_left_wall = False
+    cell5.draw(100,100, 150,150)
+
+    cell4.draw_move(cell5)
+
+    cell6 = Cell(win)
+    cell6.has_right_wall = False
+    cell6.draw(50,100, 100,150)
+
+    cell5.draw_move(cell6, True)
+
     win.wait_for_close()
 
 
