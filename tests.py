9d4207cc125de9fcfe33d8fa59a3330957ac5242
Tishues <BradleyBounds@outlook.com>
2025-04-05 22:21:42 -0500

    unit test for _break_entrance_and_exit()

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 import unittest
 from maze import Maze
 
-class TestMaze(unittest.TestCase):
+class Tests(unittest.TestCase):
     def test_maze_create_cells(self):
         num_cols = 12
         num_rows = 10
@@ -16,6 +16,13 @@ class TestMaze(unittest.TestCase):
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
         self.assertEqual(len(m1._cells), num_cols,)
         self.assertEqual(len(m1._cells[0]), num_rows,)
+
+    def test_break_entrance_and_exit(self):
+        num_cols = 10
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(m1._cells[0][0].has_top_wall, False,)
+        self.assertEqual(m1._cells[num_cols - 1][num_rows -1].has_bottom_wall, False,)
 
 
 
